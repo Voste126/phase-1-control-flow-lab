632910162c9e39e9b3b722dd9ed9558f1bd5e0ab
voste126 <steveaustine126@gmail.com>
2023-06-12 16:46:13 +0300

    functions

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if (ride > 400 && ride <= 2000) {
+    return 'That will be $10.';
+  } else if (ride > 2000 && ride <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+ return city === 'NYC'? "Ok, sounds good.": "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(message){
   // Write your code here!
+  let tip;
+
+  switch (message) {
+    case 'generous':
+      tip = "Thank you so much.";
+      break;
+    case 'not as generous':
+      tip = "Thank you.";
+    break;
+    default:
+      return "Bye.";
+  }
+
+  return tip;
 }
+//console.log(switchOnCharmFromTip('generous'));
